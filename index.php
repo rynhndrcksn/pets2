643b45ef7634e47b1e34de13f857a43193576b3d
Ryan H <rhendrickson11@mail.greenriver.edu>
2021-01-29 15:09:02 -0500

    added .gitignore, .htaccess, composer.json, and index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @authors Shawn Potter & Ryan Hendrickson
+ * @version https://github.com/rynhndrcksn/pets2
+ * more pets
+ */
+
+// This is our Controller
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+$f3 = Base::instance();
+
+// turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+$f3->route('GET /', function () {
+	echo 'My Pets';
+//    $view = new Template();
+//    echo $view->render("views/home.html");
+});
+
+$f3->run();
+
